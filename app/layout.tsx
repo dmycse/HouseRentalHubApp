6cae04599d7a67ec476e559f632ba8add9a4af82
dmycse <dmycse@gmail.com>
2024-07-10 16:55:38 +0300

    Refactor root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,27 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
+import Header from "./components/header/Header";
+import Modal from "./components/modals/Modal";
+
 const nunitoFont = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HouseRentalHub",
   description: "House rental for holiday",
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={nunitoFont.className}>{children}</body>
+      <body className={nunitoFont.className}>
+        <Modal isOpen title="Hello World!" />
+        <Header/>
+        {children}
+      </body>
     </html>
   );
 }
